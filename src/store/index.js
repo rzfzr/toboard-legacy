fabb86af82c319da04d57c7cc18191854de73cd1
Rafael Goncalves <capitao.rafa@gmail.com>
2021-04-29 18:19:31 -0300

    Hardcoded one project

--- src/store/index.js
+++ src/store/index.js
@@ -5,6 +5,11 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    projects: [{
+      Title: "Exercise",
+      id: 161873998,
+      sum: 0
+    }],
     timeEntries: []
   },
   mutations: {
--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,25 +1,40 @@
 <template>
-  <div>Dashboard</div>
+  <div>
+    <div v-for="project in this.$store.state.projects" :key="project.id">
+      {{ project.sum / 60 }} minutes
+    </div>
+  </div>
 </template>
-
 <script>
 export default {
   name: "Dashboard",
   components: {},
+  data() {
+    return {};
+  },
   created() {
+    console.log(this.$store.state.time);
     if (this.$store.state.timeEntries.length == 0) {
       console.log("getting timeEntries");
       this.$toggl.getTimeEntries(null, null, (err, timeEntries) => {
         if (err) {
           console.log("error: ", err);
         } else {
-          console.log("recieved: ", timeEntries);
+          console.log("recieved: ", timeEntries.length);
           this.$store.commit("setTimeEntries", timeEntries);
         }
       });
     } else {
       console.log("not getting timeEntries");
     }
+
+    this.$store.state.timeEntries.forEach((entry) => {
+      if (entry.pid == 161873998) {
+        this.$store.state.projects[0].sum += entry.duration;
+      }
+    });
+
+    console.log(this.$store.state.projects);
   },
 };
 </script>
